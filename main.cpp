1e9dcbc0457cbcbdbc544fb8a4fe316979f58035
hammou_h <hamza.hammouche@epitech.eu>
2018-01-09 10:56:45 +0100

    Add Mac version in main

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@ int main() {
 #ifdef _WIN32
     std::cout << "Hello, World windows!" << std::endl;
     return 0;
-#endif
-
-#ifdef linux
+#elif linux
     std::cout << "Hello, World linux!" << std::endl;
+    return 0;
+#elif __APPLE__
+    std::cout << "Hello, World Mac!" << std::endl;
     return 0;
 #endif
 }
