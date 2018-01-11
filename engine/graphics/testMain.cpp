1a29af4316436a8c9e38adeed991c3f7a08fe817
Quentin Goinaud <armaldio@gmail.com>
2018-01-11 21:50:48 +0100

    add testMain for graphics lib

---
+++ engine/graphics/testMain.cpp
@@ -0,0 +1,29 @@
+/*****************************************************************
+** Created by Quentin GOINAUD <armaldio@gmail.com> on 11/01/2018.*
+** Copyright (c) 2018 Armaldio - All rights reserved.            *
+*****************************************************************/
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
