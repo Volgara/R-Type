e583445c0eb144288df47db873002bf4762400fd
hammou_h <hamza.hammouche@epitech.eu>
2018-01-14 10:20:36 +0100

    Add unit test Box

--- unit/engine/physics/RigidBodyComponentUnit.cpp
+++ unit/engine/physics/RigidBodyComponentUnit.cpp
@@ -84,5 +84,53 @@ namespace engine {
             component->move();
             ASSERT_TRUE(component->checkIntersect(component2));
         }
+
+        TEST(BoxTest, intersectTestLeft) {
+            Vector2d pos(20, 18);
+            Vector2d velocity(-4, 3);
+            Vector2d size(30, 12);
+            RigidBodyComponent *component = new RigidBodyComponent(pos, velocity, size);
+            Vector2d pos2(30, 18);
+            Vector2d velocity2(-4, 0);
+            Vector2d size2(30, 12);
+            RigidBodyComponent *component2 = new RigidBodyComponent(pos2, velocity2, size2);
+
+            for (int i = 0; i < 11; ++i) {
+                component2->move();
+                if (i < 10)
+                    ASSERT_TRUE(component->checkIntersect(component2));
+            }
+            ASSERT_EQ(-14, component2->getPosition().getX());
+            ASSERT_EQ(50, component->getBox()->getRight());
+            ASSERT_FALSE(component->checkIntersect(component2));
+            component->move();
+            ASSERT_TRUE(component->checkIntersect(component2));
+        }
+
+        TEST(BoxTest, intersectTestTop) {
+            Vector2d pos(20, 18);
+            Vector2d velocity(0, -7);
+            Vector2d size(30, 12);
+            RigidBodyComponent *component = new RigidBodyComponent(pos, velocity, size);
+            Vector2d pos2(30, 18);
+            Vector2d velocity2(0, -3);
+            Vector2d size2(30, 12);
+            RigidBodyComponent *component2 = new RigidBodyComponent(pos2, velocity2, size2);
+
+            for (int i = 0; i < 11; ++i) {
+                component2->move();
+                if (i < 4)
+                    ASSERT_TRUE(component->checkIntersect(component2));
+            }
+            ASSERT_EQ(-15, component2->getPosition().getY());
+            ASSERT_EQ(-3, component2->getBox()->getBottom());
+            ASSERT_FALSE(component->checkIntersect(component2));
+            for (int j = 0; j < 3; ++j) {
+                component->move();
+                if (j < 2)
+                    ASSERT_FALSE(component->checkIntersect(component2));
+            }
+            ASSERT_TRUE(component->checkIntersect(component2));
+        }
     }
 }
