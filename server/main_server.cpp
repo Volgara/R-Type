062f70a83395e5287bc89f74085638e949b30f3c
alais_m <martin.alais@epitech.eu>
2018-01-21 12:59:31 +0100

    Auto stash before merge of "GameLogicServer" and "origin/GameLogicServer"

--- server/main_server.cpp
+++ server/main_server.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "Server.hpp"
 
-/*int main()
+int main()
 {
     RTypeServer::Server *serv = new RTypeServer::Server();
 
     serv->configure();
     serv->run();
     return(0);
-}*/
+}
