94c3309fa3b297b6d9d88edc2aa5de8a0e5590c2
hammou_h <hamza.hammouche@epitech.eu>
2018-01-21 17:20:47 +0100

    Fix merge

--- game/GameLogicClientSystem.cpp
+++ game/GameLogicClientSystem.cpp
@@ -3,10 +3,6 @@
 //
 
 #include "GameLogicClientSystem.hpp"
-<<<<<<< HEAD
-#include "Message.hpp"
-=======
->>>>>>> 4fcbf692e2494b4a0ba20117eb300cca20968a44
 
 game::GameLogicClientSystem::GameLogicClientSystem() {}
 
@@ -18,7 +14,6 @@ void game::GameLogicClientSystem::Update(float dt) {
 void game::GameLogicClientSystem::Init() {}
 
 void game::GameLogicClientSystem::onNotify(engine::core::Message message) {
-<<<<<<< HEAD
     if (message.id == game::GameMessageID::PLAYER_ACTION) {
         auto *eg = engine::core::Engine::GetInstance();
         auto *movePlayer = static_cast<game::PlayerActionMessage *>(&message);
@@ -36,31 +31,10 @@ void game::GameLogicClientSystem::onNotify(engine::core::Message message) {
 }
 
 void game::GameLogicClientSystem::playAction(engine::core::GameObject *source, PlayerActionMessage::Action action) {
-=======
-    if (message.id == engine::core::MessageID::MOVE_PLAYER) {
-        auto *eg = engine::core::Engine::GetInstance();
-        auto *movePlayer = static_cast<engine::core::MovePlayer *>(&message);
-
-        engine::core::GameObject *source = eg->getScene()->GetGameObject(movePlayer->source);
-        this->movePlayer(source, movePlayer->direction);
-    }
-    else if (message.id == engine::core::MessageID::MOVE) {
-        auto *eg = engine::core::Engine::GetInstance();
-        auto *move = static_cast<engine::core::Move *>(&message);
-
-        engine::core::GameObject *source = eg->getScene()->GetGameObject(move->source);
-        this->move(source, move->x, move->y);
-    }
-}
-
-void game::GameLogicClientSystem::movePlayer(engine::core::GameObject *source, engine::core::Direction dir) {
->>>>>>> 4fcbf692e2494b4a0ba20117eb300cca20968a44
     if (source->HasComponent(engine::core::ComponentID::PHY_RIGIDBODY)) {
         auto *body = dynamic_cast<engine::physics::RigidBodyComponent *>(source->GetComponent(engine::core::ComponentID::PHY_RIGIDBODY));
         auto *player = dynamic_cast<game::PlayerComponent *>(source->GetComponent(engine::core::ComponentID::GAME_PLAYER));
         engine::core::Vector2d vel(0, 0);
-
-<<<<<<< HEAD
         switch (action) {
             case game::PlayerActionMessage::Action::GO_UP:
                 vel.setY(-player->getSpeed());
@@ -72,19 +46,6 @@ void game::GameLogicClientSystem::movePlayer(engine::core::GameObject *source, e
                 vel.setX(player->getSpeed());
                 break;
             case game::PlayerActionMessage::Action::GO_LEFT:
-=======
-        switch (dir) {
-            case engine::core::UP:
-                vel.setY(-player->getSpeed());
-                break;
-            case engine::core::DOWN:
-                vel.setY(player->getSpeed());
-                break;
-            case engine::core::RIGHT:
-                vel.setX(player->getSpeed());
-                break;
-            case engine::core::LEFT:
->>>>>>> 4fcbf692e2494b4a0ba20117eb300cca20968a44
                 vel.setX(-player->getSpeed());
                 break;
         }
